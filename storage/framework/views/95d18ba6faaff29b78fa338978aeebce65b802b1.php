859f7a75c62384b665639ef45b30e8918fb10f80
Balor94 <axellai94@gmail.com>
2017-02-12 20:48:56 +0800

    Add files via upload

---
+++ storage/framework/views/95d18ba6faaff29b78fa338978aeebce65b802b1.php
@@ -0,0 +1,95 @@
+<?php $__env->startSection('title', 'Open Ticket'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Open New Ticket</div>
+
+                <div class="panel-body">
+                    <?php echo $__env->make('includes.flash', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+
+                    <form class="form-horizontal" role="form" method="POST" action="<?php echo e(url('/new_ticket')); ?>">
+                        <?php echo csrf_field(); ?>
+
+
+                        <div class="form-group<?php echo e($errors->has('title') ? ' has-error' : ''); ?>">
+                            <label for="title" class="col-md-4 control-label">Title</label>
+
+                            <div class="col-md-6">
+                                <input id="title" type="text" class="form-control" name="title" value="<?php echo e(old('title')); ?>">
+
+                                <?php if($errors->has('title')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('title')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group<?php echo e($errors->has('category') ? ' has-error' : ''); ?>">
+                            <label for="category" class="col-md-4 control-label">Category</label>
+
+                            <div class="col-md-6">
+                                <select id="category" type="category" class="form-control" name="category">
+                                    <option value="">Select Category</option>
+                                    <?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <option value="<?php echo e($category->id); ?>"><?php echo e($category->name); ?></option>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                </select>
+
+                                <?php if($errors->has('category')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('category')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group<?php echo e($errors->has('priority') ? ' has-error' : ''); ?>">
+                            <label for="priority" class="col-md-4 control-label">Priority</label>
+
+                            <div class="col-md-6">
+                                <select id="priority" type="" class="form-control" name="priority">
+                                    <option value="">Select Priority</option>
+                                    <option value="low">Low</option>
+                                    <option value="medium">Medium</option>
+                                    <option value="high">High</option>
+                                </select>
+
+                                <?php if($errors->has('priority')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('priority')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group<?php echo e($errors->has('message') ? ' has-error' : ''); ?>">
+                            <label for="message" class="col-md-4 control-label">Message</label>
+
+                            <div class="col-md-6">
+                                <textarea rows="10" id="message" class="form-control" name="message"></textarea>
+
+                                <?php if($errors->has('message')): ?>
+                                    <span class="help-block">
+                                        <strong><?php echo e($errors->first('message')); ?></strong>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-ticket"></i> Open Ticket
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
---
+++ storage/framework/views/d51efc3bc386f769c3b36b6c6307387861d626a6.php
@@ -0,0 +1,28 @@
+<?php $__env->startSection('title', 'Dashboard'); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    <p>You are logged in!</p>
+
+                    <?php if(Auth::user()->is_admin): ?>
+                        <p>
+                            See all <a href="<?php echo e(url('admin/tickets')); ?>">tickets</a>
+                        </p>
+                    <?php else: ?>
+                        <p>
+                            See all your <a href="<?php echo e(url('my_tickets')); ?>">tickets</a> or <a href="<?php echo e(url('new_ticket')); ?>">open new ticket</a>
+                        </p>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
